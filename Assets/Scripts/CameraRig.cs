0b51621de916fb033530e85771e325eec2803253
AnotherCat <artmuk.for.game@gmail.com>
2016-03-17 19:01:41 +0700

    add some new scripts

---
+++ Assets/Scripts/CameraRig.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRig : MonoBehaviour {
+
+    public float m_DampTime = 0.2f;
+    public float m_ScreenEdgeBuffer = 4f;
+    public float m_MinSize = 6.5f;
+    //[HideInInspector]
+    public Transform[] m_Targets;
+
+    private Camera m_Camera;
+    private float m_ZoomSpeed;
+    private Vector3 m_MoveVelocity;
+    private Vector3 m_DesiredPosition;
+
+    private void Awake()
+    {
+        m_Camera = GetComponent<Camera>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        Zoom();
+    }
+
+    private void Move()
+    {
+        FindAveragePosition();
+
+        transform.position = Vector3.SmoothDamp(transform.position, m_DesiredPosition, ref m_MoveVelocity, m_DampTime);
+    }
+
+    private void FindAveragePosition()
+    {
+        Vector3 averagePos = new Vector3();
+        int numTargets = 0;
+
+        for (int i = 0; i < m_Targets.Length; i++)
+        {
+            if (!m_Targets[i].gameObject.activeSelf)
+                continue;
+
+            averagePos += m_Targets[i].position;
+            numTargets++;
+        }
+
+        if (numTargets > 0)
+            averagePos /= numTargets;
+
+        averagePos.z = transform.position.z;
+
+        m_DesiredPosition = averagePos;
+    }
+
+    private void Zoom()
+    {
+        float requiredSize = FindRequiredSize();
+        m_Camera.orthographicSize = Mathf.SmoothDamp(m_Camera.orthographicSize, requiredSize, ref m_ZoomSpeed, m_DampTime);
+    }
+
+    private float FindRequiredSize()
+    {
+        Vector3 desiredLocalPos = transform.InverseTransformPoint(m_DesiredPosition);
+
+        float size = 0f;
+
+        for (int i = 0; i < m_Targets.Length; i++)
+        {
+            if (!m_Targets[i].gameObject.activeSelf)
+                continue;
+
+            Vector3 targetLocalpos = transform.InverseTransformPoint(m_Targets[i].position);
+
+            Vector3 desiredPosToTarget = targetLocalpos - desiredLocalPos;
+
+            size = Mathf.Max(size, Mathf.Abs(desiredPosToTarget.y));
+
+            size = Mathf.Max(size, Mathf.Abs(desiredPosToTarget.x) / m_Camera.aspect);
+        }
+
+        size += m_ScreenEdgeBuffer;
+
+        size = Mathf.Max(size, m_MinSize);
+
+        return size;
+    }
+
+    public void SetStartPosition()
+    {
+        FindAveragePosition();
+
+        transform.position = m_DesiredPosition;
+
+        m_Camera.orthographicSize = FindRequiredSize();
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    void Start()
+    {
+        Screen.orientation = ScreenOrientation.Portrait;
+        Input.gyro.enabled = true;
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+    }
+}
