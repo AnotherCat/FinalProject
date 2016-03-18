c81a6cdfd0e57af8a1d633564869a1fc671291f0
AnotherCat <artmuk.for.game@gmail.com>
2016-03-18 15:50:17 +0700

    Add Some Particle

---
+++ Assets/Scripts/EnemyPlane.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyPlane : MonoBehaviour {
+
+    public float Speed = 195;
+    public float shiftSpeed = 200;
+
+    public Transform missile;
+
+    Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        rb2d.AddForce(new Vector2(0, Speed * Time.deltaTime));
+
+        if (transform.position.x <= missile.transform.position.x && transform.position.x > missile.transform.position.x - 3)
+        {
+            rb2d.AddForce(new Vector2(shiftSpeed * -1 * Time.deltaTime, 0));
+        }else if(transform.position.x > missile.transform.position.x + 1 && transform.position.x < missile.transform.position.x + 3)
+        {
+            rb2d.AddForce(new Vector2(shiftSpeed * Time.deltaTime, 0));
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMissile.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMissile : MonoBehaviour {
+
+    public float Speed = 200;
+    public float shiftSpeed = 200;
+    public float bootsMultiply = 1;
+
+    bool GameOver = false;
+    Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        rb2d.AddForce(new Vector2(0, Speed * Time.deltaTime * bootsMultiply));
+
+#if UNITY_EDITOR
+        if(Input.GetKey(KeyCode.LeftArrow) && !GameOver)
+        {
+            rb2d.AddForce(new Vector2(shiftSpeed * -1 * Time.deltaTime * bootsMultiply,0));
+        }else if(Input.GetKey(KeyCode.RightArrow) & !GameOver)
+        {
+            rb2d.AddForce(new Vector2(shiftSpeed * Time.deltaTime * bootsMultiply,0));
+        }
+        if(Input.GetKeyDown(KeyCode.Space) && !GameOver)
+        {
+            bootsMultiply = 2;
+        }
+#endif
+    }
+
+    void OnCollisionEnter2D(Collision2D c)
+    {
+        Debug.Log("Booms!!!");
+        GameOver = true;
+    }
+	
+}
