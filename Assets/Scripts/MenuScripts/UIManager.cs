d2e36536c7888c598688657893efe916dcf9030e
AnotherCat <artmuk.for.game@gmail.com>
2016-03-27 01:48:40 +0700

    Love Cat

---
+++ Assets/Scripts/MenuScripts/UIManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour {
+
+    public static UIManager instance;
+
+    public Canvas GameOverCanvas;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    public void OnGameOver()
+    {
+        GameOverCanvas.enabled = true;
+    }
+    
+    public void OnRestartButtonClicked()
+    {
+        SceneManager.LoadScene("Scene1");
+    }
+
+    
+}
