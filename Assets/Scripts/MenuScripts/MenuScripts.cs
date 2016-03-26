cdcafc93e8d9a4d3892573b7259aba1558ea4eb3
Kawisara Issama-ael <Kawisara Issama-ael>
2016-03-27 01:51:21 +0700

    asma

---
+++ Assets/Scripts/MenuScripts/MenuScripts.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuScripts : MonoBehaviour {
+
+	public void nextScene () {
+        Application.LoadLevel("Scene1");
+    }	
+	
+}
