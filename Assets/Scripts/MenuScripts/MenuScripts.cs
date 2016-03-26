87564ad2272b69f68774c48e990271e747c46857
AnotherCat <artmuk.for.game@gmail.com>
2016-03-27 02:03:48 +0700

    add CAT

--- Assets/Scripts/MenuScripts/MenuScripts.cs
+++ Assets/Scripts/MenuScripts/MenuScripts.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MenuScripts : MonoBehaviour {
 
 	public void nextScene () {
-        Application.LoadLevel("Scene1");
+        //Application.LoadLevel("Scene1");
+        SceneManager.LoadScene("Scene1");
     }	
 	
 }
